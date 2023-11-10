6f6cf0bb8c2f830f802566dbd5e5eadadf92a4f0
rafaelxb <rfxbarbosa@gmail.com>
2023-11-10 17:14:18 -0300

    test: Add tests

--- TddCalc/Services/Calculator.cs
+++ TddCalc/Services/Calculator.cs
@@ -8,5 +8,30 @@ namespace TddCalc.Services
 {
     public class Calculator
     {
+
+        public int Sum(int num1, int num2)
+        {
+            return 0;
+        }
+
+        public int Minus(int num1, int num2)
+        {
+            return 0;
+        }
+
+        public int Times(int num1, int num2)
+        {
+            return 0;
+        }
+
+        public int Divide(int num1, int num2)
+        {
+            return 0;
+        }
+
+        public List<string> LastThreeCalcs()
+        {
+            return new List<string>();
+        }
     }
 }
--- TddCalcTest/CalculatorTests.cs
+++ TddCalcTest/CalculatorTests.cs
@@ -1,11 +1,48 @@
+using TddCalc.Services;
+
 namespace TddCalcTest
 {
     public class CalculatorTests
     {
-        [Fact]
-        public void Test1()
+        private Calculator _calc;
+        public CalculatorTests()
         {
+            _calc = new Calculator();
+        }
+        [Fact]
+        public void TestSumMethod()
+        {
+            Assert.Equal(6, _calc.Sum(4,2));
+        }
 
+        [Fact]
+        public void TestMinusMethod()
+        {
+            Assert.Equal(2, _calc.Minus(4, 2));
+        }
+
+        [Fact]
+        public void TestTimesMethod()
+        {
+            Assert.Equal(8, _calc.Times(4, 2));
+        }
+
+        [Fact]
+        public void TestDivideMethod()
+        {
+            Assert.Equal(2, _calc.Divide(4, 2));
+        }
+
+        [Fact]
+        public void TestDivideByZeroException()
+        {
+            Assert.Throws<DivideByZeroException>(() => _calc.Divide(4,0));
+        }
+
+        [Fact]
+        public void TestLastThreeCalcs()
+        {
+            Assert.NotEmpty(_calc.LastThreeCalcs());
         }
     }
 }
